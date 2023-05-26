60fbc0abec75dad8c8ea96efb38fd3087ca09a30
Lucas Sousa Borges <33643193+Lucsborges@users.noreply.github.com>
2023-05-25 23:22:19 -0300

    Update preprocess.cpp
    Co-authored-by: andressalayse  <alayse10@gmail.com>

--- Montador/src/preprocess.cpp
+++ Montador/src/preprocess.cpp
@@ -3,8 +3,7 @@
 int preprocess(string filenamein, string filenameout){
     fstream file(filenamein);
 	ofstream output(filenameout);
-	string line, newline;
-	string rotulo = "";
+	string line, newline, auxiliar, rotulo;
 
 	//Testa se os arquivos foram abertos com sucesso
 	if(!file.is_open()){
@@ -17,40 +16,84 @@ int preprocess(string filenamein, string filenameout){
 	}
 
 	//Pré-processamento
-	while(getline(file, line)){			//Loop principal, percorre cada linha do arquivo
-		while(line[0] == ' '){						//Retira espaços no começo da linha
-			line.erase(0, 1);
-		}
-		if(rotulo.size()){
-			line = rotulo + " " + line;		//Rotulo seguido de dois pontos e enter, pt2		
-			rotulo.clear();
-		}
 
-		for(int i = 0; i < int(line.size()); i++){ //Loop secundário, percorre cada caractere da linha
-			if(line[i] == ';'){						//Retira todos os comentários
-				break;
+	while(getline(file, line)){				                        //Loop principal, percorre cada linha do arquivo
+		
+		int verifica = 0;
+
+		for(int i = 0; i < int(line.size()); i++){                  //Loop secundário, percorre cada caractere da linha
+			
+			line.erase(std::remove_if(line.begin(), line.end(), [](char caracter){      //Remove linhas vazias
+				return std::isspace(caracter) && caracter != ' '; }), line.end());
+
+			if(line[i] == ' ' && line[i+1] == ':'){					//Tira espaço antes de dois pontos
+				line.erase(i, 1);
+			}
+			if(line[i] == ',' && line[i+1] != ' '){                 //Espaço antes e depois da vírgula
+				line.insert(i+1, 1, ' ');
+			}
+			else if(line[i] != ' ' && line[i+1] == ','){
+				line.insert(i+1, 1, ' ');
 			}
 
-			if((line[i] == ' ') && ((line[i+1] == ' ') || (line[i+1] == '\n') || (line[i+1] == ';'))){//Retira espaços no meio da linha
-				continue;
+			if(line[i] == ':' && line[i+1] == ' ' && (!isalpha(line[i+2]))){      //Rotulo seguido de dois pontos e enter pt.1
+				verifica++;
+				auxiliar = line.substr(0, i+1);
+				//rotulo = line.substr(0, i);
 			}
-			newline.push_back(toupper(line[i]));		//Transforma tudo em maiúsculo
+			if(verifica!=0){
+				line.clear();
+				line = auxiliar;
+				getline(file, newline);
+				line = auxiliar + " " + newline;		//Rotulo seguido de dois pontos e enter pt.2
+				auxiliar.clear();
+				newline.clear();
+				verifica=0;
+				break;		
+			}
+		
+			if(line[i] == ';'){							//Retira todos os comentários
+				int j = i;
+				while(j < int(line.size())){
+					line[j] = ' ';
+					j++;
+				}
+			}
+						
+			while(line[0] == ' '){						//Retira espaços no começo da linha
+				line.erase(0, 1);
+			} 
+
+			for(char &caracter : line){
+        		caracter = std::toupper(caracter);      //Transforma tudo em maiúsculo
+   			}
+		}
+		
+		std::istringstream ler(line);
+        std::ostringstream juntar;	
+        std::string proxima;
+
+		int verifica2 = 0;
+        int palavra = 1;
+        
+        while(ler >> proxima){                         //Tira espaços no meio da linha
+            if(palavra == 1){
+                juntar << proxima;
+                palavra = 0;
+            }else {
+                juntar << ' ' << proxima;
+            }
+			verifica2 = 1;
+        }
+
+		if(verifica2 == 1){
+			output << juntar.str() << std::endl;       //Linhas com espaços removidos do meio
+		}
+		else if(int(line.size())){
+			output << line << endl;         
 		}
 
-		while (newline.back() == ' '){		//Retira espaços no final da linha
-			newline.pop_back();
-		}
-
-		if(newline.back() == ':'){ //rotulo seguido de dois pontos e ENTER, pt1
-			rotulo += newline;
-			newline.clear();		
-		}
-
-		if(newline.size()){ //Remove linhas vazias
-			output << newline << endl;
-		}
-		newline.clear();
-		line.clear();
+		line.clear();	
 	}
 
 	//Fecha arquivos
@@ -74,8 +117,8 @@ corretamente) (DONE)
 linha seguinte (dica coloca na mesma linha no arquivo pre-processado) (DONE)
 
 - O comando COPY deve separar os argumentos por “,” COM espaços antes e depois 
-da “,”(ex: COPY A1 , A2) (PENDING)
+da “,”(ex: COPY A1 , A2) (OK)
 
 - SEMPRE tera UM espaço no mínimo entre TOKENS, com exceção do “:” (ex. R1: 
-INPUT N1) (PENDING)
+INPUT N1) (OK)
 */
